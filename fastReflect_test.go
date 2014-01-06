d434cfa121bdff2e7f43532cc9d7d4fb508f6473
fanliao <liaofan1973@gmail.com>
2014-01-06 12:38:49 +0800

    add unsafe set testing

--- fastReflect_test.go
+++ fastReflect_test.go
@@ -11,6 +11,26 @@ type RWTestStruct struct {
 	Name string
 }
 
+func TestReflect(t *testing.T) {
+	o := &RWTestStruct{}
+	p := interface{}(o)
+	//Below failed
+	//p1 := *interface{}(o)
+	t.Log(o, p)
+}
+
+func BenchmarkTypeCast(b *testing.B) {
+	o := &RWTestStruct{}
+	var o2 *RWTestStruct
+	for i := 0; i < b.N; i++ {
+		var o1 interface{}
+		o1 = o
+		o2 = o1.(*RWTestStruct)
+	}
+	b.StopTimer()
+	b.Log(o2)
+}
+
 func BenchmarkFastGeter(b *testing.B) {
 	o := &RWTestStruct{1, "test"}
 	p := unsafe.Pointer(o)
@@ -57,7 +77,7 @@ func BenchmarkFastGeterByIndexButNoFunc(b *testing.B) {
 	b.Log(id, name)
 }
 
-func BenchmarkUnsafe(b *testing.B) {
+func BenchmarkUnsafeGet(b *testing.B) {
 	o := &RWTestStruct{1, "test"}
 	p := unsafe.Pointer(o)
 	offsetId := unsafe.Offsetof(o.Id)
@@ -71,42 +91,6 @@ func BenchmarkUnsafe(b *testing.B) {
 	}
 	b.StopTimer()
 	b.Log(id, name)
-}
-
-func BenchmarkUnsafeSet(b *testing.B) {
-	o := &RWTestStruct{1, "test"}
-	p := unsafe.Pointer(o)
-	offsetId := unsafe.Offsetof(o.Id)
-	OffsetName := unsafe.Offsetof(o.Name)
-	//var id int
-	//var name string
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		//id = *((*int)(unsafe.Pointer(uintptr(p) + offsetId)))
-		//idPtr := ((*int)(unsafe.Pointer(uintptr(p) + offsetId)))
-		//*idPtr = 11
-		*((*int)(unsafe.Pointer(uintptr(p) + offsetId))) = 11
-		//name = *((*string)(unsafe.Pointer(uintptr(p) + OffsetName)))
-		*((*string)(unsafe.Pointer(uintptr(p) + OffsetName))) = "test unsafe set"
-
-	}
-	b.StopTimer()
-	//id = o.Id
-	//name = o.Name
-	b.Log(o)
-}
-
-func BenchmarkReflectSet(b *testing.B) {
-	o := &RWTestStruct{1, "test"}
-	v := reflect.ValueOf(o).Elem()
-	//t := v.Type()
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		v.Field(0).SetInt(11)
-		v.Field(1).SetString("test reflect set")
-	}
-	b.StopTimer()
-	b.Log(o.Id, o.Name)
 }
 
 func BenchmarkGeter(b *testing.B) {
@@ -160,4 +144,65 @@ func BenchmarFastSeter(b *testing.B) {
 		rw.Set(p, "Id", 2)
 		rw.Set(p, "Name", "testSet")
 	}
+}
+
+func BenchmarkUnsafeSet(b *testing.B) {
+	o := &RWTestStruct{1, "test"}
+	p := unsafe.Pointer(o)
+	offsetId := unsafe.Offsetof(o.Id)
+	OffsetName := unsafe.Offsetof(o.Name)
+	//var id int
+	//var name string
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		//id = *((*int)(unsafe.Pointer(uintptr(p) + offsetId)))
+		//idPtr := ((*int)(unsafe.Pointer(uintptr(p) + offsetId)))
+		//*idPtr = 11
+		*((*int)(unsafe.Pointer(uintptr(p) + offsetId))) = 11
+		//name = *((*string)(unsafe.Pointer(uintptr(p) + OffsetName)))
+		*((*string)(unsafe.Pointer(uintptr(p) + OffsetName))) = "test unsafe set"
+
+	}
+	b.StopTimer()
+	//id = o.Id
+	//name = o.Name
+	b.Log(o)
+}
+
+func BenchmarkReflectSet(b *testing.B) {
+	o := &RWTestStruct{1, "test"}
+	v := reflect.ValueOf(o).Elem()
+	//t := v.Type()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		v.Field(0).SetInt(11)
+		v.Field(1).SetString("test reflect set")
+	}
+	b.StopTimer()
+	b.Log(o.Id, o.Name)
+}
+
+func BenchmarkFastSeterByIndexButNoFunc(b *testing.B) {
+	o := &RWTestStruct{1, "test"}
+	p := unsafe.Pointer(o)
+	rw := getFastRWer(o, p)
+	meta := rw.GetStructMeta()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		*((*int)(unsafe.Pointer(uintptr(p) + meta.FieldOffsetsByIndex[0]))) = 11
+		*((*string)(unsafe.Pointer(uintptr(p) + meta.FieldOffsetsByIndex[1]))) = "test"
+	}
+	b.StopTimer()
+	b.Log(o.Id, o.Name)
+}
+
+func BenchmarkSeter(b *testing.B) {
+	o := &RWTestStruct{1, "test"}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		o.Id = 11
+		o.Name = "test"
+	}
+	b.StopTimer()
+	b.Log(o.Id, o.Name)
 }
